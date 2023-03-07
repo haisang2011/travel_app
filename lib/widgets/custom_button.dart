24253032a344dd530db8b50c8b85d1e6abd53153
Dong Nguyen <nvdkg1999@gmail.com>
2023-03-07 10:04:03 +0700

    Add common widget custom button

---
+++ lib/widgets/custom_button.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+import '../constants/colors.dart';
+
+class CustomButton extends StatelessWidget {
+  final BorderRadiusGeometry? borderRadius;
+  final double? width;
+  final double? height;
+  final Color? backgroundColor;
+  final Gradient? gradient;
+  final VoidCallback onPressed;
+  final Widget child;
+
+  const CustomButton({
+    Key? key,
+    required this.onPressed,
+    required this.child,
+    this.borderRadius,
+    this.backgroundColor,
+    this.width = double.infinity,
+    this.height,
+    this.gradient = const LinearGradient(
+      colors: [ColorPalette.primaryColor, ColorPalette.secondaryColor],
+      begin: Alignment.topLeft,
+      end: Alignment.bottomRight,
+    ),
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final borderRadius = this.borderRadius ?? BorderRadius.circular(21.sp);
+    final height = this.height ?? 5.h;
+    return Container(
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+        gradient: gradient,
+        borderRadius: borderRadius,
+      ),
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          elevation: 0,
+          backgroundColor: backgroundColor ?? Colors.transparent,
+          shadowColor: Colors.transparent,
+          shape: RoundedRectangleBorder(borderRadius: borderRadius),
+        ),
+        child: child,
+      ),
+    );
+  }
+}
