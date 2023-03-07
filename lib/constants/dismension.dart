b321d6cc1ea09a1074be72d0c1d964f3387ae1cb
Sang Nguyen <nguyen.sang.98.107@gmail.com>
2023-03-07 11:37:18 +0700

    add dismension.dart file

---
+++ lib/constants/dismension.dart
@@ -0,0 +1,11 @@
+const double kMediumPadding = 24.0;
+const double kBottomBarIconSize = 20.0;
+const double kDefaultPadding = 16.0;
+const double kItemPadding = 10.0;
+const double kTopPadding = 8.0;
+
+const double kDefaultIconSize = 18.0;
+
+const double kSmallFontSize = 14.0;
+const double kDefaultFontSize = 16.0;
+const double kMediumFontSize = 24.0;
